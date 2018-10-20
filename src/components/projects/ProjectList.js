ebda9781497f040bd47e0730c7113d1e5d1b30e9
anas140 <muhammedanas4140@gmail.com>
2018-10-20 16:48:29 +0530

    Project Summery

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummery from './ProjectSummery'
 
 const ProjectList = () => {
 	return (
 		<div className="project-list section">
-			<div className="card z-depth-0 project-summery"> 
-				<div className="card-content grey-text text-darken-3">
-					<span className="cart-title">Project Title</span>
-					<p>Posted By Muhammed Anas</p>
-					<p className="grey-text">3rd September 2am</p>
-				</div>
-			</div>
-
-			<div className="card z-depth-0 project-summery"> 
-				<div className="card-content grey-text text-darken-3">
-					<span className="cart-title">Project Title</span>
-					<p>Posted By Muhammed Anas</p>
-					<p className="grey-text">3rd September 2am</p>
-				</div>
-			</div>
-
-			<div className="card z-depth-0 project-summery"> 
-				<div className="card-content grey-text text-darken-3">
-					<span className="cart-title">Project Title</span>
-					<p>Posted By Muhammed Anas</p>
-					<p className="grey-text">3rd September 2am</p>
-				</div>
-			</div>
+			<ProjectSummery />
+			<ProjectSummery />
+			<ProjectSummery />
+			<ProjectSummery />
 		</div>
 	)
 }
---
+++ src/components/projects/ProjectSummery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummery = () => {
+	return (
+		<div className="card z-depth-0 project-summery"> 
+			<div className="card-content grey-text text-darken-3">
+				<span className="cart-title">Project Title</span>
+				<p>Posted By Muhammed Anas</p>
+				<p className="grey-text">3rd September 2am</p>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectSummery
