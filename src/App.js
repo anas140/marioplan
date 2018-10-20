fa5442218bf3eaecacf0fd12e479c3f7c49dd91b
anas140 <muhammedanas4140@gmail.com>
2018-10-20 14:50:58 +0530

    NavBar>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     return (
     	<BrowserRouter>
 	      <div className="App">
-	        <h1 className="center red-text">
-	          Mario Plan
-	        </h1>
+	        <Navbar />
 	      </div>
       </BrowserRouter>
     );
