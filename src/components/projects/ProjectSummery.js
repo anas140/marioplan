7c10391ca24c8e52be442cc3412b568cbcd9a46f
anas140 <muhammedanas4140@gmail.com>
2018-10-24 16:59:09 +0530

    Project Details

--- src/components/projects/ProjectSummery.js
+++ src/components/projects/ProjectSummery.js
@@ -4,7 +4,7 @@ const ProjectSummery = () => {
 	return (
 		<div className="card z-depth-0 project-summery"> 
 			<div className="card-content grey-text text-darken-3">
-				<span className="cart-title">Project Title</span>
+				<span className="card-title">Project Title</span>
 				<p>Posted By Muhammed Anas</p>
 				<p className="grey-text">3rd September 2am</p>
 			</div>
