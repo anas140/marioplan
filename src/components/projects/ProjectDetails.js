1aa73aa0f6ad9462e35eb709e90138f5dd97e07d
anas140 <muhammedanas4140@gmail.com>
2018-10-29 16:58:53 +0530

    project details

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,3 +1,25 @@
 import React from 'react';
 
-const ProjectDetails = 
+const ProjectDetails = () => {
+	return (
+		<div className="container section project-details">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<div className="card-title">Project Title</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div className="card-action grey lighten-4 grey-text">
+					<div>Posted by the net ninja</div>
+					<div>2nd September, 2am</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectDetails
